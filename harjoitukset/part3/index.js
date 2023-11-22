b10f2a08ceaccf66271bea2f6dd97b4a62836489
Erika Avikainen <avikainen@Erikas-MacBook.local>
2023-11-22 14:34:52 +0200

    notes internettiin

--- harjoitukset/part3/index.js
+++ harjoitukset/part3/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
