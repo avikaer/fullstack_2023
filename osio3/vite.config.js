15e20900f4e28b01f925a92944c2de666d9d1303
Erika Avikainen <avikainen@Erikas-MacBook.local>
2024-02-19 13:35:52 +0200

    korjattu versio, toimiva build

---
+++ osio3/vite.config.js
@@ -0,0 +1,16 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+export default {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve('osio3', 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: '/src/index.js',
+    },
+  },
+};
